720fefc3d4c149859aa7570bf0968ff6b6307c98
Ritesh Kudalkar <kriteshkudalkar@gmail.com>
2020-05-22 20:01:17 +0530

    Initial Commit

---
+++ game.js
@@ -0,0 +1,81 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var gameStarted = false;
+var gamePatternIndex = 0;
+
+function startOver() {
+  gamePattern = []; //console.log(failure);
+  userClickedPattern = [];
+  gamePatternIndex = 0;
+  level = 0;
+  gameStarted = false;
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  //button animation from computer
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+
+  //button sound from computer
+  playSound(randomChosenColor);
+
+  userClickedPattern = [];
+  gamePatternIndex = 0;
+}
+
+$(".btn").click(function(event) {
+  var userChosenColor = event.target.id; //console.log(event.target.id); or console.log(this.attr("id"));
+  userClickedPattern.push(userChosenColor);
+
+  //button sound on user click
+  playSound(userChosenColor);
+
+  //button animation on user click
+  animatePress(userChosenColor);
+
+  if(userChosenColor === gamePattern[gamePatternIndex]) {
+    gamePatternIndex++; //console.log("success!");
+    if(level === gamePatternIndex) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    startOver();
+    
+    $("h1").text("Game Over, Press Any Key to Restart");
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+  }
+});
+
+//Start Game
+$(document).keydown(function(event) {
+  if (!gameStarted) {
+    gameStarted = true;
+    nextSequence();
+  }
+});
